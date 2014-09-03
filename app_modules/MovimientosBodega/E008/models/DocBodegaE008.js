69c10f3facf1f7b91818c0c7cfc07a5a01911bb2
Camilo Orozco <corozco@ecodev.com.co>
2014-09-03 16:21:50 -0500

    Se implementa servicios realtime para los docuemntos temporales

--- app_modules/MovimientosBodega/E008/models/DocBodegaE008.js
+++ app_modules/MovimientosBodega/E008/models/DocBodegaE008.js
@@ -115,6 +115,7 @@ DocuemntoBodegaE008.prototype.consultar_documentos_temporales_clientes = functio
     var sql = " select \
                 a.doc_tmp_id as documento_temporal_id,\
                 a.usuario_id,\
+                b.bodegas_doc_id,\
                 a.pedido_cliente_id as numero_pedido,\
                 d.tipo_id_tercero as tipo_id_cliente, \
                 d.tercero_id as identificacion_cliente, \
@@ -224,29 +225,41 @@ DocuemntoBodegaE008.prototype.consultar_documentos_temporales_farmacias = functi
 // Consultar documento temporal de clientes x numero de pedido
 DocuemntoBodegaE008.prototype.consultar_documento_temporal_clientes = function(numero_pedido, callback) {
 
-    var sql = " select\
-                a.doc_tmp_id,\
+    var sql = " select \
+                a.doc_tmp_id as documento_temporal_id,\
+                a.usuario_id,\
                 b.bodegas_doc_id,\
                 a.pedido_cliente_id as numero_pedido,\
-                a.tipo_id_tercero as tipo_id_cliente,\
-                a.tercero_id as identificacion_cliente,\
+                d.tipo_id_tercero as tipo_id_cliente, \
+                d.tercero_id as identificacion_cliente, \
                 d.nombre_tercero as nombre_cliente, \
                 d.direccion as direccion_cliente, \
-                d.telefono as telefono_cliente, \
+                d.telefono as telefono_cliente,\
                 e.tipo_id_vendedor, \
                 e.vendedor_id as idetificacion_vendedor, \
                 e.nombre as nombre_vendedor, \
-                c.estado,  \
-                a.observacion,\
-                b.fecha_registro,\
-                a.usuario_id\
+                c.estado,\
+                case when c.estado = 0 then 'Inactivo' \
+                     when c.estado = 1 then 'Activo' end as descripcion_estado,\
+                c.estado_pedido as estado_actual_pedido, \
+                case when c.estado_pedido = 0 then 'No Asignado' \
+                     when c.estado_pedido = 1 then 'Asignado' \
+                     when c.estado_pedido = 2 then 'Auditado' \
+                     when c.estado_pedido = 3 then 'En Despacho' \
+                     when c.estado_pedido = 4 then 'Despachado'\
+                     when c.estado_pedido = 5 then 'Despachado con Pendientes' \
+                     when c.estado_pedido = 6 then 'En Auditoria' end as descripcion_estado_actual_pedido,    \
+                a.estado as estado_separacion,     \
+                case when a.estado = '0' then 'Separacion en Proceso' \
+                     when a.estado = '1' then 'Separacion Finalizada' \
+                     when a.estado = '2' then 'En Auditoria' end as descripcion_estado_separacion,     \
+                c.fecha_registro,    \
+                b.fecha_registro as fecha_separacion_pedido \
                 from inv_bodegas_movimiento_tmp_despachos_clientes a\
                 inner join inv_bodegas_movimiento_tmp b on a.doc_tmp_id = b.doc_tmp_id and a.usuario_id = b.usuario_id\
                 inner join ventas_ordenes_pedidos c on a.pedido_cliente_id = c.pedido_cliente_id\
                 inner join terceros d on c.tipo_id_tercero = d.tipo_id_tercero and c.tercero_id = d.tercero_id \
                 inner join vnts_vendedores e on c.tipo_id_vendedor = e.tipo_id_vendedor and c.vendedor_id = e.vendedor_id \
-                inner join ventas_ordenes_pedidos_estado f on c.pedido_cliente_id = f.pedido_cliente_id and c.estado_pedido = f.estado\
-                inner join operarios_bodega g on f.responsable_id = g.operario_id \
                 where a.pedido_cliente_id = $1 ";
 
     G.db.query(sql, [numero_pedido], function(err, rows, result) {
@@ -260,29 +273,39 @@ DocuemntoBodegaE008.prototype.consultar_documento_temporal_clientes = function(n
 DocuemntoBodegaE008.prototype.consultar_documento_temporal_farmacias = function(numero_pedido, callback) {
 
     var sql = " select \
-                a.doc_tmp_id,\
+                a.doc_tmp_id as documento_temporal_id,\
+                a.usuario_id,\
                 b.bodegas_doc_id,\
-                c.solicitud_prod_a_bod_ppal_id as numero_pedido,\
-                c.farmacia_id, \
-                d.empresa_id, \
+                a.solicitud_prod_a_bod_ppal_id as numero_pedido,\
+                c.farmacia_id,\
+                f.empresa_id,\
                 c.centro_utilidad,\
                 c.bodega as bodega_id,\
-                f.razon_social as nombre_farmacia, \
+                f.razon_social as nombre_farmacia,\
                 d.descripcion as nombre_bodega,\
-                a.usuario_id, \
-                g.nombre as nombre_usuario ,\
-                c.estado,\
-                a.observacion,\
-                to_char(b.fecha_registro, 'dd-mm-yyyy') as fecha_registro, \
-                b.fecha_registro as fecha_separacion_pedido \
-                from inv_bodegas_movimiento_tmp_despachos_farmacias a \
+                c.usuario_id as usuario_genero,\
+                g.nombre as nombre_usuario,\
+                c.estado as esta_actual_pedido,\
+                case when c.estado = 0 then 'No Asignado' \
+                     when c.estado = 1 then 'Asignado' \
+                     when c.estado = 2 then 'Auditado' \
+                     when c.estado = 3 then 'En Despacho' \
+                     when c.estado = 4 then 'Despachado' \
+                     when c.estado = 5 then 'Despachado con Pendientes' \
+                     when c.estado = 6 then 'En Auditoria' end as descripcion_estado_actual_pedido, \
+                a.estado as estado_separacion,\
+                case when a.estado = '0' then 'Separacion en Proceso' \
+                     when a.estado = '1' then 'Separacion Finalizada' \
+                     when a.estado = '2' then 'En Auditoria' end as descripcion_estado_separacion,   \
+                c.fecha_registro,\
+                b.fecha_registro as fecha_separacion_pedido\
+                from inv_bodegas_movimiento_tmp_despachos_farmacias a\
                 inner join inv_bodegas_movimiento_tmp b on a.doc_tmp_id = b.doc_tmp_id and a.usuario_id = b.usuario_id\
-                inner join solicitud_productos_a_bodega_principal c on a.solicitud_prod_a_bod_ppal_id = c.solicitud_prod_a_bod_ppal_id \
-                inner join bodegas d on c.farmacia_id = d.empresa_id and c.centro_utilidad = d.centro_utilidad and c.bodega = d.bodega\
-                inner join empresas f ON d.empresa_id = f.empresa_id\
-                inner join system_usuarios g ON a.usuario_id = g.usuario_id\
-                inner join solicitud_productos_a_bodega_principal_estado h on c.solicitud_prod_a_bod_ppal_id = h.solicitud_prod_a_bod_ppal_id and c.estado = h.estado\
-                inner join operarios_bodega i on h.responsable_id = i.operario_id\
+                inner join solicitud_productos_a_bodega_principal c on a.solicitud_prod_a_bod_ppal_id = c.solicitud_prod_a_bod_ppal_id\
+                inner join bodegas d on c.farmacia_id = d.empresa_id and c.centro_utilidad = d.centro_utilidad and c.bodega = d.bodega \
+                inner join centros_utilidad e on d.empresa_id = e.empresa_id and d.centro_utilidad = e.centro_utilidad \
+                inner join empresas f ON e.empresa_id = f.empresa_id \
+                inner join system_usuarios g ON c.usuario_id = g.usuario_id \
                 where a.solicitud_prod_a_bod_ppal_id = $1 ";
 
     G.db.query(sql, [numero_pedido], function(err, rows, result) {

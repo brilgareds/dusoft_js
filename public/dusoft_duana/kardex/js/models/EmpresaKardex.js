ff59e4fae72f8d3626f40b5bfa749668a5101fc8
egarcia <desarrollo@duanaltda.com>
2015-01-15 14:56:09 -0500

    cambiando nombre archivo

---
+++ public/dusoft_duana/kardex/js/models/EmpresaKardex.js
@@ -0,0 +1,39 @@
+
+define(["angular", "js/models","includes/classes/Empresa"], function(angular, models) {
+
+    models.factory('EmpresaKardex',['Empresa', function(Empresa) { 
+       
+       function EmpresaKardex(nombre, codigo){
+           Empresa.getClass().call(this,nombre, codigo);
+       }
+       
+        EmpresaKardex.prototype = Object.create(Empresa.getClass().prototype);
+       
+        EmpresaKardex.prototype.productos = [];
+
+
+        EmpresaKardex.prototype.setClientes = function() {
+
+        };
+
+        EmpresaKardex.prototype.agregarProducto = function(producto) {
+            this.productos.push(producto);
+        };
+
+        EmpresaKardex.prototype.getProductos = function() {
+            return this.productos;
+        };
+
+        EmpresaKardex.prototype.vaciarProductos = function() {
+            this.productos = [];
+        };
+        
+        this.get = function(nombre, codigo){
+            return new EmpresaKardex(nombre, codigo);
+        };
+
+
+        return this;
+
+    }]);
+});

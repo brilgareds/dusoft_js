185e72a86f3925b6db53e930796d94f10a7a927d
egarcia <desarrollo@duanaltda.com>
2015-02-12 11:31:40 -0500

    corrigiendo tamaño fuente pdf despachos

--- public/dusoft_duana/pedidos/js/controllers/auditoriapedidos/AuditoriaPedidosFarmaciasController.js
+++ public/dusoft_duana/pedidos/js/controllers/auditoriapedidos/AuditoriaPedidosFarmaciasController.js
@@ -170,7 +170,27 @@ define(["angular", "js/controllers",
 
             //se realiza el llamado a api para pedidos
             $scope.buscarPedidosSeparados(that.obtenerParametros(), 2, false, $scope.renderPedidosSeparados);
-            $scope.listarEmpresas("");   
+            $scope.listarEmpresas("");
+            
+            
+            
+             var test = {
+                session: $scope.session,
+                data: {
+                    movimientos_bodegas: {
+                        empresa: '03',
+                        numero: '82913',
+                        prefijo: 'EFC'
+                    }
+                }
+            };
+            Request.realizarRequest(API.DOCUMENTOS_DESPACHO.IMPRIMIR_DOCUMENTO_DESPACHO, "POST", test, function(data) {
+                if(data.status === 200){
+                    var nombre = data.obj.movimientos_bodegas.nombre_pdf;
+                    $scope.visualizarReporte("/reports/" + nombre, nombre, "download");
+                }
+                
+            });
 
         }]);
 });

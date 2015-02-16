8c4c7cb897be017f69dd3fce9985fbb849a7d819
egarcia <desarrollo@duanaltda.com>
2015-02-16 16:34:05 -0500

    trabajando en view de parametrizacion de modulos

---
+++ public/dusoft_duana/parametrizacion/js/controllers/AdministracionModulos/Modulos/AdministracionModulosController.js
@@ -0,0 +1,16 @@
+
+define(["angular", "js/controllers", "js/models"], function(angular, controllers) {
+
+    controllers.controller('AdministracionModulosController', ['$scope', '$rootScope', 'Request', '$modal', 'API',
+        "socket", "$timeout", "AlertService", "Usuario",
+        function($scope, $rootScope, Request, $modal, API, socket, $timeout, AlertService, Usuario) {
+
+
+            $scope.listado_opciones = {
+                
+            };
+
+
+            
+        }]);
+});
---
+++ public/dusoft_duana/parametrizacion/js/controllers/AdministracionPerfiles/Roles/AdministracionRolesController.js
@@ -0,0 +1,10 @@
+
+define(["angular", "js/controllers", "js/models"], function(angular, controllers) {
+
+    controllers.controller('AdministracionRolesController', ['$scope', '$rootScope', 'Request', '$modal', 'API',
+        "socket", "$timeout", "AlertService", "Usuario",
+        function($scope, $rootScope, Request, $modal, API, socket, $timeout, AlertService, Usuario) {
+
+            
+        }]);
+});

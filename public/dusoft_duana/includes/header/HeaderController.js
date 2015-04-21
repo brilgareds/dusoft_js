b39295720e1419ce4fa916a98055f3552a6f15f2
egarcia <desarrollo@duanaltda.com>
2015-04-21 11:55:22 -0500

    trabajando en parametrizacion del usuario

--- public/dusoft_duana/includes/header/HeaderController.js
+++ public/dusoft_duana/includes/header/HeaderController.js
@@ -6,50 +6,57 @@ define(["angular", "js/controllers", "includes/classes/Usuario","includes/Consta
         function($scope, $rootScope, $state,
         Request, Usuario, socket, URL, localStorageService,Empresa) {
             var self = this;
-            var obj = localStorageService.get("session");
-            console.log("session obj ", obj)
-            if(!obj) return;
+            var obj_session = localStorageService.get("session");
+            console.log("session obj_session ", obj_session)
+            if(!obj_session) return;
             
-            setUsuarioActual(obj);
+           // setUsuarioActual(obj_session);
 
                 
             $scope.mostarLock = false;
             $scope.unlockform = {};
             
-            $scope.obj = {
+            $scope.obj_session = {
                 usuario:"",
                 clave:""
             };
             
             var session = {
-                usuario_id: $scope.Usuario.getId(),
-                auth_token: $scope.Usuario.getToken()
+                usuario_id:obj_session.usuario_id,
+                auth_token: obj_session.auth_token
             };
             
             $scope.empresas = [];
            
             
-           function setUsuarioActual(obj){
-                var usuario = Usuario.get(obj.usuario_id, obj.detalle.usuario, obj.detalle.nombre);
+           self.setUsuarioActual = function(obj){
+                var usuario = Usuario.get(obj.usuario_id, obj.usuario, obj.nombre);
                 
                 usuario.setRutaAvatar("/images/avatar_empty.png");
                         
-                usuario.setToken(obj.auth_token);
+                usuario.setToken(session.auth_token);
                 usuario.setUsuarioId(obj.usuario_id); 
                 
-                if(obj.detalle.ruta_avatar && obj.detalle.ruta_avatar.length > 0){
+                if(obj.ruta_avatar && obj.ruta_avatar.length > 0){
                     
-                    usuario.setRutaAvatar(URL.CONSTANTS.STATIC.RUTA_AVATAR+usuario.getId() + "/" +obj.detalle.ruta_avatar);
+                    usuario.setRutaAvatar(URL.CONSTANTS.STATIC.RUTA_AVATAR+usuario.getId() + "/" +obj.ruta_avatar);
                 }
                 
-                usuario.setNombre(obj.detalle.nombre);
-                usuario.setNombreUsuario(obj.detalle.usuario);
-                usuario.setEmail(obj.detalle.email);
+                usuario.setNombre(obj.nombre);
+                usuario.setNombreUsuario(obj.usuario);
+                usuario.setEmail(obj_session.email);
                 
-                var empresa = Empresa.get("", obj.detalle.empresa_id);
+                var empresa_id = obj_session.empresa_id;
+                
+                if(!empresa_id){
+                    empresa_id = obj.empresa_id;
+                }
+                
+                var empresa = Empresa.get("", empresa_id);
                 usuario.setEmpresa(empresa);
                 
-                console.log("ruta de avatar ", usuario.getRutaAvatar())
+                
+                console.log("ruta de avatar ", usuario.getRutaAvatar());
                 Usuario.setUsuarioActual(usuario);
 
                 $scope.Usuario = usuario;
@@ -92,6 +99,31 @@ define(["angular", "js/controllers", "includes/classes/Usuario","includes/Consta
                     
                 });
             };
+            
+            
+            self.traerUsuarioPorId = function(usuario_id, callback){
+
+                var obj = {
+                    session: session,
+                    data: {
+                        parametrizacion_usuarios: {
+                            usuario_id: usuario_id
+                        }
+                    }
+                };
+                
+                Request.realizarRequest(URL.CONSTANTS.API.USUARIOS.OBTENER_USUARIO_POR_ID, "POST", obj, function(data) {
+                    var obj = data.obj.parametrizacion_usuarios.usuario;
+                    
+                    if(obj){
+                        
+                        self.setUsuarioActual(obj);
+                        callback(obj);
+                    }
+                    
+                });
+            };
+            
                         
                
            self.traerParametrizacionPorUsuario = function(empresa_id, callback){
@@ -131,7 +163,9 @@ define(["angular", "js/controllers", "includes/classes/Usuario","includes/Consta
                             }
                         
                         }
-                        if(modulos.length > 0){
+                        
+                        console.log("modulos del sistema ", _modulos);
+                        if(_modulos.length > 0){
                             //estando los modulos preparados se envian al controlador del menu                      
                             $rootScope.$emit("modulosUsuario", _modulos);
                         }
@@ -147,12 +181,8 @@ define(["angular", "js/controllers", "includes/classes/Usuario","includes/Consta
                 self.traerParametrizacionPorUsuario($scope.Usuario.getEmpresa().getCodigo(),function(parametrizacion){
                     
                     var obj = localStorageService.get("session");
-                    console.log("parametrizacion ", parametrizacion)
-                    console.log("session ", obj);
                     
-                    obj.detalle.empresa_id = parametrizacion.rol.empresa_id;
-                    obj.detalle.login_empresas_id =  parametrizacion.rol.login_empresa_id;
-                    obj.detalle.id_rol = parametrizacion.rol.id;
+                    obj.empresa_id = parametrizacion.rol.empresa_id;
                     
                     localStorageService.add("session", JSON.stringify(obj));
                     location.reload();
@@ -184,18 +214,17 @@ define(["angular", "js/controllers", "includes/classes/Usuario","includes/Consta
 
             $scope.autenticar = function(){
 
-                console.log($scope.obj.clave)
                 var session = {
                     usuario_id: Usuario.usuario_id,
                     auth_token: Usuario.token
                 };
 
-                Request.realizarRequest('/api/unLockScreen', "POST", {session: session, data: {login:{contrasenia:$scope.obj.clave}}}, function(data) {
+                Request.realizarRequest('/api/unLockScreen', "POST", {session: session, data: {login:{contrasenia:$scope.obj_session.clave}}}, function(data) {
                     if(data.status === 200){
                         $scope.msgerror = "";
                         $scope.mostrarmensaje = false;
                         $scope.mostarLock = false;
-                        $scope.obj = {};
+                        $scope.obj_session = {};
                     } else {
                         $scope.msgerror = data.msj;
                         $scope.mostrarmensaje = true;
@@ -214,11 +243,22 @@ define(["angular", "js/controllers", "includes/classes/Usuario","includes/Consta
                 Request.realizarRequest('/api/lockScreen', "POST", {session: session, data: {}}, function(data) {
                     if(data.status === 200){
                         $scope.mostarLock = true;
-                        $scope.obj = {};
+                        $scope.obj_session = {};
                     }
 
                 });
             };
+            
+            
+          $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+                    /*console.log("event === ", event);
+                    console.log("toState === ", toState);
+                    console.log("toParams === ", toParams);
+                    console.log("fromState === ", fromState);
+                    console.log("fromParams === ", fromParams);*/
+                    //event.preventDefault();
+                    
+           });
 
             socket.on("onCerrarSesion",function(){
                 console.log("onCerrarSesion");
@@ -230,24 +270,32 @@ define(["angular", "js/controllers", "includes/classes/Usuario","includes/Consta
             //evento de coneccion al socket
             socket.on("onConnected", function(datos){
                 var socketid = datos.socket_id;
-                var obj = {
+                var obj_session = {
                   usuario_id : Usuario.usuario_id,
                   auth_token : Usuario.token,
                   socket_id : socketid
                 };
 
-                socket.emit("onActualizarSesion",obj);
+                socket.emit("onActualizarSesion",obj_session);
              });   
             
-            
+                            
+            var empresa_id = obj_session.empresa_id;
 
-            self.traerParametrizacionPorUsuario($scope.Usuario.getEmpresa().getCodigo(),function(parametrizacion){
-                self.obtenerEmpresasUsuario(function(){
-                    $rootScope.$emit("parametrizacionUsuarioLista", parametrizacion);
-                });
+            if(!empresa_id){
+                empresa_id = $scope.Usuario.getEmpresa().getCodigo();
+            }
+            
+            self.traerUsuarioPorId(obj_session.usuario_id, function(){
                 
-            });
-            
+                self.traerParametrizacionPorUsuario(empresa_id,function(parametrizacion){
+                    self.obtenerEmpresasUsuario(function(){
+                        console.log("usuario ", $scope.Usuario);
+                        $rootScope.$emit("parametrizacionUsuarioLista", parametrizacion);
+                    });
 
+                });
+            });
+           
         }]);
 });

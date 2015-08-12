9f43884b903da78606350062bb9adaa9a32105ea
egarcia <desarrollo@duanaltda.com>
2015-08-12 17:57:06 -0500

    trabajando en permisos

--- public/dusoft_duana/pedidos/js/controllers/generacionpedidos/pedidosfarmacias/PedidosFarmaciaController.js
+++ public/dusoft_duana/pedidos/js/controllers/generacionpedidos/pedidosfarmacias/PedidosFarmaciaController.js
@@ -21,10 +21,31 @@ define(["angular",
             var self = this;
             
             $scope.root = {};
+            var self = this;
             $scope.root.empresasFarmacias = Usuario.getUsuarioActual().getEmpresasFarmacias();
-                        
+            
+            /*
+             * @Author: Eduar
+             * @param {object} obj
+             * +Descripcion: Handler del boton crear pedido
+             */
             $scope.onIrVistaGuardarPedidoTemporal = function(){
                 $state.go('GuardarPedidoTemporal');
+            };
+            
+            self.opciones = Usuario.getUsuarioActual().getModuloActual().opciones;
+            
+            //permisos kardex
+            $scope.root.opcionesModulo = {
+                btnCrearPedido: {
+                    'click': self.opciones.sw_crear_pedido
+                },
+                btnVerPedido: {
+                    'click': self.opciones.sw_consultar_pedido 
+                },
+                btnModificarPedido: {
+                    'click': self.opciones.sw_consultar_pedido  && self.opciones.sw_modificar_pedido
+                }        
             };
 
         }]);

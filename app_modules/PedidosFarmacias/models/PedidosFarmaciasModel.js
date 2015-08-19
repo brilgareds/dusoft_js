6e683558249ff566e046228ee3768de673e73574
egarcia <desarrollo@duanaltda.com>
2015-08-19 08:59:34 -0500

    trabajando en issue postgresql 8.3

--- app_modules/PedidosFarmacias/models/PedidosFarmaciasModel.js
+++ app_modules/PedidosFarmacias/models/PedidosFarmaciasModel.js
@@ -820,7 +820,7 @@ PedidosFarmaciasModel.prototype.terminar_estado_pedido = function(numero_pedido,
     estados = estados.join(",");
     
     var sql = "update solicitud_productos_a_bodega_principal_estado set sw_terminado = $2\
-               where solicitud_prod_a_bod_ppal_id = $1 and estado in("+estados+") and (sw_terminado is null or sw_terminado = '0')";
+               where solicitud_prod_a_bod_ppal_id  = $1 and estado in('"+estados+"') and (sw_terminado is null or sw_terminado = '0')";
 
     G.db.query(sql, [numero_pedido, terminado], function(err, rows, result) {
         callback(err, rows, result);

94ed22689c38a32790b42f96400e13819ad28bfc
egarcia <desarrollo@duanaltda.com>
2015-08-29 09:27:06 -0500

    trabajando en dropdown de pedidos farmacias

--- app_modules/PedidosFarmacias/models/PedidosFarmaciasModel.js
+++ app_modules/PedidosFarmacias/models/PedidosFarmaciasModel.js
@@ -555,7 +555,7 @@ PedidosFarmaciasModel.prototype.listar_pedidos_farmacias = function(empresa_id, 
     }).
     limit(G.settings.limit).
     offset((pagina - 1) * G.settings.limit).
-    orderByRaw("1 asc").
+    orderByRaw("1 DESC").
     then(function(rows){
         console.log("resultado de farmacias >>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",rows);
         callback(false, rows);

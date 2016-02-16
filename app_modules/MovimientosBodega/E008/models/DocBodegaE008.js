d020d40f05aeea39a19fd4fe39d7723ea9ea86bf
egarcia <desarrollo@duanaltda.com>
2016-02-16 16:37:07 -0500

    ok

--- app_modules/MovimientosBodega/E008/models/DocBodegaE008.js
+++ app_modules/MovimientosBodega/E008/models/DocBodegaE008.js
@@ -664,11 +664,11 @@ DocuemntoBodegaE008.prototype.consultarNumeroMayorRotulo = function(documento_id
 
 // Consultar el rotulo de una caja 
 DocuemntoBodegaE008.prototype.consultar_rotulo_caja = function(documento_id, numero_caja, numero_pedido, callback) {
-    var sql = " select * from inv_rotulo_caja a where /* a.documento_id = :1 and */ numero_caja = :2 and solicitud_prod_a_bod_ppal_id = :3 and (sw_despachado = '0' or sw_despachado is null); ";
+    var sql = " select * from inv_rotulo_caja a where  a.documento_id = :1 and  numero_caja = :2 and solicitud_prod_a_bod_ppal_id = :3 and (sw_despachado = '0' or sw_despachado is null); ";
 
         
-   G.knex.raw(sql, {/*1:documento_id, */2:numero_caja, 3:numero_pedido}).
-   then(function(resultado){
+   G.knex.raw(sql, {1:documento_id, 2:numero_caja, 3:numero_pedido}).
+   then(function(resultado){  
        callback(false, resultado.rows, resultado);
    }).catch(function(err){
        callback(err);

49e55097d2b75df74efd5e6d706e774fa470c2fc
egarcia <desarrollo@duanaltda.com>
2016-03-02 16:16:22 -0500

    upgrade de redis

--- app_modules/Pedidos/cronjobs/PedidosCronJobs.js
+++ app_modules/Pedidos/cronjobs/PedidosCronJobs.js
@@ -10,17 +10,6 @@ PedidosCronJobs.prototype.liberarReservas = function() {
 
     var that = this;
     
-    //var cron = new G.cronJob({
-        //cronTime: '* * * * *',
-        //cronTime: '*/5 * * * * *',
-       /* onTick: function() {
-            console.log(">>>>>>>>>>>>>>>>>>>>>>>>>> liberar Reservas");
-        },
-        start: false
-    });
-    cron.start();*/
-    
-    
     var job = new G.cronJob('*/5 * * * * *', function () {
         // Do some stuff here 
         console.log(">>>>>>>>>>>>>>>>>>>>>>>>>> liberar Reservas");

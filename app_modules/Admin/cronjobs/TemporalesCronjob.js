809ef628698d4d10f4311f3a521003e00f2dfd21
egarcia <desarrollo@duanaltda.com>
2016-04-27 08:24:24 -0500

    ajuste de cronjobs

--- app_modules/Admin/cronjobs/TemporalesCronjob.js
+++ app_modules/Admin/cronjobs/TemporalesCronjob.js
@@ -1,13 +1,17 @@
 var TemporalesCronjob = function() {
    var that = this;
-   that.iniciar();
+   //Cronjob solo corren en modo produccion
+   if(G.program.prod){
+        that.iniciar();
+   }
 
 };
 
 TemporalesCronjob.prototype.iniciar = function(req, res){
     var that = this;
     //El cronjob correra todos los dias a media noche
-    var job = new G.cronJob('*/59 */59 */23 * * *', function () {
+    //var job = new G.cronJob('*/59 */59 */23 * * *', function () {
+    var job = new G.cronJob('*/59 */25 */8 * * *', function () {
         console.log("corriendo crontab para borrar temporales");
         G.utils.limpiarDirectorio(G.dirname + "/public/reports/");
         G.utils.limpiarDirectorio(G.dirname + "/files/tmp/");

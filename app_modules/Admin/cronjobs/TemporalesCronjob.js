450b38bb03eebd1147a142af60ad7ab0f4f3d12a
egarcia <desarrollo@duanaltda.com>
2016-04-27 10:20:01 -0500

    ok

--- app_modules/Admin/cronjobs/TemporalesCronjob.js
+++ app_modules/Admin/cronjobs/TemporalesCronjob.js
@@ -11,8 +11,8 @@ var TemporalesCronjob = function() {
 TemporalesCronjob.prototype.iniciar = function(req, res){
     var that = this;
     //El cronjob correra todos los dias a media noche
-    //var job = new G.cronJob('*/59 */59 */23 * * *', function () {
-    var job = new G.cronJob('* * * * * *', function () {
+    var job = new G.cronJob('*/59 */59 */23 * * *', function () {
+    //var job = new G.cronJob('* * * * * *', function () {
         console.log("corriendo crontab para borrar temporales");
         G.utils.limpiarDirectorio(G.dirname + "/public/reports/");
         G.utils.limpiarDirectorio(G.dirname + "/files/tmp/");

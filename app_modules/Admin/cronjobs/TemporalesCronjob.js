834a43e04e75a1eed4852c5ea2f14c77d1871372
egarcia <desarrollo@duanaltda.com>
2016-05-12 15:00:53 -0500

    logica de borrar directorios pasado a utils

--- app_modules/Admin/cronjobs/TemporalesCronjob.js
+++ app_modules/Admin/cronjobs/TemporalesCronjob.js
@@ -6,33 +6,16 @@ var TemporalesCronjob = function() {
 
 TemporalesCronjob.prototype.iniciar = function(req, res){
     var that = this;
-    
+    //El cronjob correra todos los dias a media noche
     var job = new G.cronJob('*/59 */59 */23 * * *', function () {
         console.log("corriendo crontab para borrar temporales");
-        that.limpiarDirectorio(G.dirname + "/public/reports/");
-        that.limpiarDirectorio(G.dirname + "/files/tmp/");
+        G.utils.limpiarDirectorio(G.dirname + "/public/reports/");
+        G.utils.limpiarDirectorio(G.dirname + "/files/tmp/");
         
     });
     job.start();
     
 };
-
-TemporalesCronjob.prototype.limpiarDirectorio = function(path) {
-  var that = this;
-  if( G.fs.existsSync(path) ) {
-    G.fs.readdirSync(path).forEach(function(file,index){
-        
-      var curPath = path + "/" + file;
-      if(G.fs.lstatSync(curPath).isDirectory()) { // recurse
-        that.limpiarDirectorio(path);
-      } else { // delete file
-        G.fs.unlinkSync(curPath);
-      }
-    });
-   //G.fs.rmdirSync(path);
-  }
-};
-
 
 
 module.exports = TemporalesCronjob;

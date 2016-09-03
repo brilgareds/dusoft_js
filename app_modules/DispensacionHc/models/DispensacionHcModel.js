6998516fc771a6bc19935df479870c90b0a4b37f
Cristian manuel <desarrollo2@duanaltda.com>
2016-09-03 12:47:15 -0500

    Ok

--- app_modules/DispensacionHc/models/DispensacionHcModel.js
+++ app_modules/DispensacionHc/models/DispensacionHcModel.js
@@ -125,7 +125,7 @@ DispensacionHcModel.prototype.listarFormulas = function(obj, callback){
                                     WHEN a.sw_pendiente = '0' OR a.sw_pendiente = '1' THEN(\
                                         CASE WHEN a.fecha_minima_entrega <= now() and  now() <= a.fecha_maxima_entrega THEN 'Entrar'\
                                         WHEN now() > a.fecha_maxima_entrega THEN 'Vencido'\
-                                        ELSE 'Falta' || EXTRACT(DAY FROM  a.fecha_minima_entrega - timestamp 'now()') || 'Dias' END\
+                                        ELSE 'Falta ' || EXTRACT(DAY FROM  a.fecha_minima_entrega - timestamp 'now()')+1 || ' Dias' END\
                                         )\
                                     WHEN a.sw_pendiente = '2' THEN 'Todo pendiente' END\
                                 ) \

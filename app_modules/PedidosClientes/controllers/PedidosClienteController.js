ba077fb47e46b15cd726d3ef3d42480f4c0e6c2e
Cristian manuel <desarrollo2@duanaltda.com>
2016-11-24 17:17:19 -0500

    Ok

--- app_modules/PedidosClientes/controllers/PedidosClienteController.js
+++ app_modules/PedidosClientes/controllers/PedidosClienteController.js
@@ -2641,8 +2641,7 @@ PedidosCliente.prototype.modificarDetallePedido = function(req, res) {
          *               estado (Estado del Pedido ) 1
          *               estado_pedido (Estado de solicitud ) 0
          */
-        if (resultado[0].estado === '1' && (resultado[0].estado_pedido === '0') 
-         || resultado[0].estado === '1' &&  resultado[0].estado_pedido === '8') {
+        if (resultado[0].estado === '1' && (resultado[0].estado_pedido === '0' || resultado[0].estado_pedido === '8')) {
             return G.Q.ninvoke(that.m_pedidos_clientes, 'consultarTotalValorPedidoCliente', numeroPedido);
         } else {
             throw ("El pedido debe estar activo o para autorizar nuevamente por cartera");

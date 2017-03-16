d92db36f2dab43c372ed15a40af1c72f1e4b62c7
egarcia <desarrollo@duanaltda.com>
2017-03-16 11:41:14 -0500

    trabajando en backend de servicios para formulario de terceros

--- app_modules/Terceros/GestionTerceros/models/TercerosModel.js
+++ app_modules/Terceros/GestionTerceros/models/TercerosModel.js
@@ -2,22 +2,73 @@ var TercerosModel = function() {
 
 };
 
-/**
- * @api {sql} listar_clientes Listar Clientes 
- * @apiName Listar Clientes
- * @apiGroup Terceros (sql)
- * @apiDescription Lista los clientes de la empresa.
- * @apiDefinePermission autenticado Requiere Autenticacion
- * Requiere que el usuario esté autenticado.
- * @apiPermission autenticado
- * @apiParam {Function} callback Funcion de retorno de informacion.
- */
-TercerosModel.prototype.obtenerParametrizacionFormularioTerceros = function(parametros, callback) {
 
+TercerosModel.prototype.obtenerParametrizacionFormularioTerceros = function(parametros, callback) {
+    var that = this;
+    var datos = {};
     
+    G.Q.ninvoke(that,'obtenerGeneros', parametros).then(function(generos) {
+        datos.generos = generos;
+        return G.Q.ninvoke(that,'obtenerTiposDocumentos', parametros);
+      
+    }).then(function(tiposDocumentos){
+        datos.tiposDocumentos = tiposDocumentos;
+        return G.Q.ninvoke(that,'obtenerTiposEstoCivil', parametros);
+        
+    }).then(function(tiposEstadoCivil){
+        datos.tiposEstadoCivil = tiposEstadoCivil;
+        return G.Q.ninvoke(that,'obtenerTiposNacionalidad', parametros);
+        
+    }).then(function(tiposNacionalidad){
+        datos.tiposNacionalidad = tiposNacionalidad;
+        return G.Q.ninvoke(that,'obtenerTiposDireccion', parametros);
+        
+    }).then(function(tiposDireccion){
+        datos.tiposDireccion = tiposDireccion;
+        return G.Q.ninvoke(that,'obtenerTiposTelefono', parametros);
+        
+    }).then(function(tiposTelefeno){
+        datos.tiposTelefeno = tiposTelefeno;
+        return G.Q.ninvoke(that,'obtenerTiposLineaTelefonica', parametros);
+        
+    }).then(function(tiposLineaTefelonica){
+        datos.tiposLineaTefelonica = tiposLineaTefelonica;
+        return G.Q.ninvoke(that,'obtenerTiposCorreo', parametros);
+        
+    }).then(function(tiposCorreo){
+        datos.tiposCorreo = tiposCorreo;
+        return G.Q.ninvoke(that,'obtenerTiposRedSocial', parametros);
+        
+    }).then(function(tiposRedSocial){
+        datos.tiposRedSocial = tiposRedSocial;
+        return G.Q.ninvoke(that,'obtenerTiposContacto', parametros);
+        
+    }).then(function(tiposContacto){
+        datos.tiposContacto = tiposContacto;
+        callback(false, datos);
+        
+    }).fail(function(err) {
+        
+        var msj = err;
+        var status = 500;
+        
+        if(err.status){
+            status = err.status;
+            msj = err.msj;
+        }
+        
+        
+        callback(err);
+    }).done();
     
 };
 
+/**
+* @author Eduar Garcia
+* +Descripcion: Obtiene los generos disponibles
+* @params obj: {params, callback}
+* @fecha 2017-03-16
+*/
 TercerosModel.prototype.obtenerGeneros = function(parametros, callback) {
     var columns = [
         "a.id",
@@ -34,6 +85,12 @@ TercerosModel.prototype.obtenerGeneros = function(parametros, callback) {
     
 };
 
+/**
+* @author Eduar Garcia
+* +Descripcion: Obtiene los tipos de documentos disponibles
+* @params obj: {params, callback}
+* @fecha 2017-03-16
+*/
 TercerosModel.prototype.obtenerTiposDocumentos = function(parametros, callback) {
     var columns = [
         "a.tipo_id_tercero",
@@ -42,8 +99,7 @@ TercerosModel.prototype.obtenerTiposDocumentos = function(parametros, callback) 
     
     G.knex.column(columns).
     from("tipo_id_terceros as a").
-    orderBy("a.indice_de_orden", "desc")
-    .then(function(tiposDocumentos){
+    orderBy("a.indice_de_orden", "desc").then(function(tiposDocumentos){
         callback(false, tiposDocumentos);
     }).catch(function(error){
         callback(error);
@@ -51,6 +107,12 @@ TercerosModel.prototype.obtenerTiposDocumentos = function(parametros, callback) 
 
 };
 
+/**
+* @author Eduar Garcia
+* +Descripcion: Obtiene los tipos de estado civil disponibles
+* @params obj: {params, callback}
+* @fecha 2017-03-16
+*/
 TercerosModel.prototype.obtenerTiposEstoCivil = function(parametros, callback) {
     var columns = [
         "a.tipo_estado_civil_id",
@@ -59,8 +121,7 @@ TercerosModel.prototype.obtenerTiposEstoCivil = function(parametros, callback) {
     
     G.knex.column(columns).
     from("tipo_estado_civil as a").
-    orderBy("a.indice_de_orden", "desc")
-    .then(function(tiposDocumentos){
+    orderBy("a.indice_de_orden", "desc").then(function(tiposDocumentos){
         callback(false, tiposDocumentos);
     }).catch(function(error){
         callback(error);
@@ -68,6 +129,12 @@ TercerosModel.prototype.obtenerTiposEstoCivil = function(parametros, callback) {
 
 };
 
+/**
+* @author Eduar Garcia
+* +Descripcion: Obtiene los tipos de nacionalidad disponibles
+* @params obj: {params, callback}
+* @fecha 2017-03-16
+*/
 TercerosModel.prototype.obtenerTiposNacionalidad = function(parametros, callback) {
     var columns = [
         "a.id",
@@ -75,15 +142,20 @@ TercerosModel.prototype.obtenerTiposNacionalidad = function(parametros, callback
     ];
     
     G.knex.column(columns).
-    from("nacionalidad as a")
-    .then(function(tiposNacionalidad){
-        callback(false, tiposNacionalidads);
+    from("nacionalidad as a").then(function(tiposNacionalidad){
+        callback(false, tiposNacionalidad);
     }).catch(function(error){
         callback(error);
     }).done();
 
 };
 
+/**
+* @author Eduar Garcia
+* +Descripcion: Obtiene los tipos de direccion disponibles
+* @params obj: {params, callback}
+* @fecha 2017-03-16
+*/
 TercerosModel.prototype.obtenerTiposDireccion = function(parametros, callback) {
     var columns = [
         "a.id",
@@ -91,8 +163,7 @@ TercerosModel.prototype.obtenerTiposDireccion = function(parametros, callback) {
     ];
     
     G.knex.column(columns).
-    from("tipo_direccion as a")
-    .then(function(tipoDireccion){
+    from("tipo_direccion as a").then(function(tipoDireccion){
         callback(false, tipoDireccion);
     }).catch(function(error){
         callback(error);
@@ -100,6 +171,12 @@ TercerosModel.prototype.obtenerTiposDireccion = function(parametros, callback) {
 
 };
 
+/**
+* @author Eduar Garcia
+* +Descripcion: Obtiene los tipos de telefono disponibles
+* @params obj: {params, callback}
+* @fecha 2017-03-16
+*/
 TercerosModel.prototype.obtenerTiposTelefono = function(parametros, callback) {
     var columns = [
         "a.id",
@@ -107,8 +184,7 @@ TercerosModel.prototype.obtenerTiposTelefono = function(parametros, callback) {
     ];
     
     G.knex.column(columns).
-    from("tipos_telefono as a")
-    .then(function(tiposTelefono){
+    from("tipos_telefono as a").then(function(tiposTelefono){
         callback(false, tiposTelefono);
     }).catch(function(error){
         callback(error);
@@ -116,107 +192,88 @@ TercerosModel.prototype.obtenerTiposTelefono = function(parametros, callback) {
 
 };
 
-
-
-TercerosModel.prototype.obtenterClientePorId = function(parametros, callback) {
-
-    var sql = " SELECT\
-                a.tipo_id_tercero,\
-                a.tercero_id,\
-                a.direccion,\
-                a.telefono,\
-                a.email,\
-                a.nombre_tercero,\
-                a.tipo_bloqueo_id\
-                FROM terceros as a\
-                LEFT JOIN inv_tipos_bloqueos b ON a.tipo_bloqueo_id = b.tipo_bloqueo_id\
-                WHERE \
-                    a.tercero_id = :1  AND \
-                    a.tipo_id_tercero = :2";
+/**
+* @author Eduar Garcia
+* +Descripcion: Obtiene los tipos de linea telefonica disponibles
+* @params obj: {params, callback}
+* @fecha 2017-03-16
+*/
+TercerosModel.prototype.obtenerTiposLineaTelefonica = function(parametros, callback) {
+    var columns = [
+        "a.id",
+        "a.descripcion"
+    ];
     
-    
-    var query = G.knex.raw(sql, {1:parametros.tercero_id, 2:parametros.tipo_id_tercero});
-    
-    query.then(function(resultado){
-       callback(false, resultado.rows);
-    }).catch(function(err){
-         console.log(" err [obtenterClientePorId] ", err);
-        callback(err);
-    });
-    
-};
-
-TercerosModel.prototype.listar_clientes_ciudad = function(empresa_id, pais_id, departamento_id, ciudad_id, termino_busqueda, callback) {
-
-    var sql = " a.tipo_id_tercero,\
-                a.tercero_id,\
-                a.nombre_tercero,\
-                g.tipo_pais_id as pais_id,\
-                g.pais as nombre_pais,\
-                f.tipo_dpto_id as departamento_id,\
-                f.departamento as nombre_departamento,\
-                e.tipo_mpio_id as ciudad_id,\
-                e.municipio as nombre_ciudad,\
-                a.direccion,\
-                a.telefono,\
-                a.email,\
-                a.tipo_bloqueo_id,\
-                c.descripcion as bloqueo\
-                from terceros a\
-                inner join terceros_clientes b on a.tipo_id_tercero = b.tipo_id_tercero AND a.tercero_id = b.tercero_id \
-                left join inv_tipos_bloqueos c on a.tipo_bloqueo_id = c.tipo_bloqueo_id\
-                left join tipo_mpios e on a.tipo_pais_id = e.tipo_pais_id AND a.tipo_dpto_id = e.tipo_dpto_id AND a.tipo_mpio_id = e.tipo_mpio_id\
-                left join tipo_dptos f on e.tipo_pais_id = f.tipo_pais_id AND e.tipo_dpto_id = f.tipo_dpto_id \
-                left join tipo_pais g on f.tipo_pais_id = g.tipo_pais_id\
-                WHERE b.empresa_id = :1 and a.tipo_pais_id = :2  and a.tipo_dpto_id= :3 and a.tipo_mpio_id= :4 and \
-                (\
-                    a.tipo_id_tercero :: varchar "+G.constants.db().LIKE+" :5 or\
-                    a.tercero_id :: varchar "+G.constants.db().LIKE+"  :5 or\
-                    a.nombre_tercero :: varchar "+G.constants.db().LIKE+" :5 \
-                )\
-                ORDER BY a.nombre_tercero ";
-
-    
-   G.knex.select(G.knex.raw(sql, {1:empresa_id, 2:pais_id, 3:departamento_id, 4:ciudad_id, 5:"%" + termino_busqueda + "%"})).
-   then(function(resultado){
-       callback(false, resultado);
-   }).catch(function(err){
-       callback(err);
-   });
+    G.knex.column(columns).
+    from("tipos_linea_telefonica as a").then(function(tiposTelefono){
+        callback(false, tiposTelefono);
+    }).catch(function(error){
+        callback(error);
+    }).done();
 
 };
 
 /**
- * @api {sql} consultar_contrato_cliente Consultar Contrato Cliente
- * @apiName Consultar Contrato Cliente
- * @apiGroup Terceros (sql)
- * @apiDescription Consulta el contrato del cliente.
- * @apiDefinePermission autenticado Requiere Autenticacion
- * Requiere que el usuario esté autenticado.
- * @apiPermission autenticado
- * @apiParam {Function} callback Funcion de retorno de informacion.
- * @apiSuccessExample Este SQL se usa en:
- *     Modulo : 
- *     Accion : 
- * @apiSuccessExample SQL.
- *   SELECT
- */
-TercerosModel.prototype.consultar_contrato_cliente = function(tipo_id_cliente, cliente_id, callback) {
-
-    var sql = " SELECT\
-                    a.tipo_id_tercero, a.tercero_id, COALESCE(b.contrato_cliente_id, 0) as contrato_cliente_id, COALESCE(b.empresa_id, '0') as empresa_id,\
-                    COALESCE(b.estado, '0') as estado\
-                FROM terceros a\
-                    LEFT JOIN vnts_contratos_clientes b on a.tipo_id_tercero = b.tipo_id_tercero and a.tercero_id = b.tercero_id\
-                WHERE a.tipo_id_tercero = :1 \
-                    AND a.tercero_id = :2 ";
+* @author Eduar Garcia
+* +Descripcion: Obtiene los tipos de correo disponibles
+* @params obj: {params, callback}
+* @fecha 2017-03-16
+*/
+TercerosModel.prototype.obtenerTiposCorreo = function(parametros, callback) {
+    var columns = [
+        "a.id",
+        "a.descripcion"
+    ];
     
-   G.knex.raw(sql, {1:tipo_id_cliente, 2:cliente_id}).
-   then(function(resultado){
-       callback(false, resultado.rows, resultado);
-   }).catch(function(err){
-       callback(err);
-   });
+    G.knex.column(columns).
+    from("tipos_correo as a").then(function(tiposCorreo){
+        callback(false, tiposCorreo);
+    }).catch(function(error){
+        callback(error);
+    }).done();
+
+};
+
+/**
+* @author Eduar Garcia
+* +Descripcion: Obtiene los tipos de red social disponibles
+* @params obj: {params, callback}
+* @fecha 2017-03-16
+*/
+TercerosModel.prototype.obtenerTiposRedSocial = function(parametros, callback) {
+    var columns = [
+        "a.id",
+        "a.descripcion"
+    ];
+    
+    G.knex.column(columns).
+    from("tipos_redes_sociales as a").then(function(tiposCorreo){
+        callback(false, tiposCorreo);
+    }).catch(function(error){
+        callback(error);
+    }).done();
+
+};
+
+
+/**
+* @author Eduar Garcia
+* +Descripcion: Obtiene los tipos de contacto disponibles
+* @params obj: {params, callback}
+* @fecha 2017-03-16
+*/
+TercerosModel.prototype.obtenerTiposContacto = function(parametros, callback) {
+    var columns = [
+        "a.id",
+        "a.descripcion"
+    ];
+    
+    G.knex.column(columns).
+    from("tipo_contacto as a").then(function(tiposContacto){
+        callback(false, tiposContacto);
+    }).catch(function(error){
+        callback(error);
+    }).done();
 
 };
 

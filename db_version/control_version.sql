325479dff8225908fb131ff729d6e406d36a07d1
Cristian manuel <desarrollo2@duanaltda.com>
2018-01-16 11:13:00 -0500

    nueva version v2.00.02

--- db_version/control_version.sql
+++ db_version/control_version.sql
@@ -21,3 +21,10 @@ BEGIN TRANSACTION;
 INSERT INTO version (version, modulo, comentario) VALUES ('02.00.01', 'SISTEMA', 'Se elimina temporalmente el socket de estaditicas del sistema por problemas con tablets');
 COMMIT TRANSACTION;
 
+-----------------------------------------------------------------------------
+------------------------------  Version 2.00.02  ------------------------------
+-----------------------------------------------------------------------------
+BEGIN TRANSACTION;
+INSERT INTO version (version, modulo, comentario) VALUES ('02.00.02', 'SISTEMA', 'Pone en funcionamiento el socket de estadisticas del sistema');
+COMMIT TRANSACTION;
+

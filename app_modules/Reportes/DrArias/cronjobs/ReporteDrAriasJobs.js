4b6e1f33c9ba54d7f4f2eb27047573c28392c3b9
andres mauricio <corozco@ecodev.com.co>
2018-01-19 08:41:34 -0500

    se modifica la consulta de dispensacion y el crontab de reortes

--- app_modules/Reportes/DrArias/cronjobs/ReporteDrAriasJobs.js
+++ app_modules/Reportes/DrArias/cronjobs/ReporteDrAriasJobs.js
@@ -6,7 +6,7 @@ var ReporteDrAriasJobs = function(m_drArias) {
     that.m_drArias = m_drArias;
     if(ip.address()==='10.0.2.229'){
 	
-	if(G.program.prod){
+	if(G.program.prod){           
 	    that.iniciar();
 	}
     }
@@ -47,21 +47,21 @@ function __InsertarDrArias(that,swCopia){
     var numeroRegistros = '0';
     G.Q.ninvoke(that.m_drArias,"conteoTemporalesReporteDrArias").then(function(result){ 
           numeroRegistros=result[0].numero;
-            if(parsetInt(numeroRegistros) === 0){
+            if(parseInt(numeroRegistros) === 0){
               return G.Q.ninvoke(that.m_drArias, "addTemporalesReporteDrArias");
              }else{
              
               jobInicio.stop();
               return;
              }           
-        }).then(function(datos){           
-            if(parsetInt(numeroRegistros) === 0){
+        }).then(function(datos){    
+            if(parseInt(numeroRegistros) === 0){
              return G.Q.ninvoke(that.m_drArias, "conteoTemporalesReporteDrArias");
             }else
              return [{numero:'1'}];            
         }).then(function(datos){
             var numeroRegistrosfinal=datos[0].numero;
-            if(parsetInt(numeroRegistrosfinal) !== 0 && parsetInt(numeroRegistros) === 0){
+            if(parseInt(numeroRegistrosfinal) !== 0 && parseInt(numeroRegistros) === 0){
             
              var update={};
              update.numero=datos[0].numero;
